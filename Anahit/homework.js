995c07e7f08e8cad290485be79d8ed82d9367299
Anahit Kocharyan <anahitkocharyan@Anahits-MacBook-Air.local>
2025-12-16 20:53:36 +0400

    saving the changes
    t push#:wq
    :wq

---
+++ Anahit/homework.js
@@ -0,0 +1,27 @@
+//let customer1 = "Svetik"
+//let salary1= 10
+
+let customer2 = "Anahit"
+let salary2 = 15
+
+let customer3 = "Sona"
+let salary3 = 20
+
+let customer4 = "Chrit"
+let salary4 = 25
+
+function count(salary) {
+    return salary *30
+    //console.log (count)
+}
+//console.log (count(salary1))
+//console.log (count(salary2))
+//console.log (count(salary3))
+//console.log (count(salary4))
+
+function adding(array, number) {
+    array.push(number)
+}
+const array = []
+adding(array,5)
+console.log(array)
